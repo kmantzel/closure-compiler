ec23b6a59a00c59b2bae7b4d222de296e08e5a8d
Kyle Mantzel <kmantzel@gmail.com>
2021-03-01 21:09:17 -0800

    Approximate mappings enabled by default

--- src/com/google/debugging/sourcemap/SourceMapConsumerV3.java
+++ src/com/google/debugging/sourcemap/SourceMapConsumerV3.java
@@ -62,7 +62,7 @@ public final class SourceMapConsumerV3 implements SourceMapConsumer,
   }
 
   public SourceMapConsumerV3() {
-    this(false);
+    this(true);
   }
 
   public SourceMapConsumerV3(boolean approximateMappings) {
